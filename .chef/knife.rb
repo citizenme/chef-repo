cb76bf3ad56de16415f37da4f241f0f64a2d0515
Morten Jensen <morten@citizenme.com>
2013-12-29 17:40:28 +0000

    Removed bunch of files from .chef

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,9 +1,10 @@
 log_level                :info
 log_location             STDOUT
-node_name                'citizenme'
-client_key               '/home/citizenme/chef-repo/.chef/citizenme.pem'
+node_name                'mjensen'
+client_key               '/home/ubuntu/chef-repo/.chef/mjensen.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/citizenme/chef-repo/.chef/chef-validator.pem'
-chef_server_url          'https://mgmt.internal.virtuability.com'
-syntax_check_cache_path  '/home/citizenme/chef-repo/.chef/syntax_check_cache'
-cookbook_path [ '/home/citizenme/chef-repo/cookbooks' ]
+validation_key           '/home/ubuntu/chef-repo/.chef/chef-validator.pem'
+chef_server_url          'https://ec2-54-194-51-0.eu-west-1.compute.amazonaws.com'
+syntax_check_cache_path  '/home/ubuntu/chef-repo/.chef/syntax_check_cache'
+cookbook_path [ '~/chef-repo/cookbooks' ]
+
