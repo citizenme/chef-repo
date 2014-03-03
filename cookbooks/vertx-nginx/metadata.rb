f3d319776d7cf5210406851b5245657e23955389
Morten Jensen <morten@citizenme.com>
2014-03-03 10:26:01 +0000

    nginx ohai dependency bumped up - added new cookbook for vertx-nginx integration

---
+++ cookbooks/vertx-nginx/metadata.rb
@@ -0,0 +1,10 @@
+name             'vertx-nginx'
+maintainer       'citizenme'
+maintainer_email 'info@citizenme.com'
+license          'Apache 2.0'
+description      'Installs/Configures vertx-nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends 'nginx',             '~> 2.4'
+
