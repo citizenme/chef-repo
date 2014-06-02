9bfb5f61206b6765bb2c0477577d55d114dd0615
Morten Jensen <morten@citizenme.com>
2014-06-02 14:53:21 +0100

    Amended repo with my personal paths in knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,12 +1,12 @@
 log_level                :info
 log_location             STDOUT
 node_name                'mjensen'
-client_key               '/home/ubuntu/.chef/mjensen.pem'
+client_key               '/home/mjensen/.chef/mjensen.pem'
 validation_client_name   'chef-validator'
-validation_key           '/home/ubuntu/.chef/chef-validator.pem'
+validation_key           '/home/mjensen/.chef/chef-validator.pem'
 chef_server_url          'https://chefserver.cloud.citizenme.com'
-syntax_check_cache_path  '/home/ubuntu/.chef/syntax_check_cache'
-cookbook_path [ '/home/ubuntu/chef-repo/cookbooks' ]
+syntax_check_cache_path  '/home/mjensen/.chef/syntax_check_cache'
+cookbook_path [ '/home/mjensen/chef-repo/cookbooks' ]
 cookbook_copyright       "citizenme"
 cookbook_email           "info@citizenme.com"
 cookbook_license         "apachev2"
