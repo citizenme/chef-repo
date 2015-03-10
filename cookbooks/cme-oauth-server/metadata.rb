70b34b2222988d958b88ce359e98e35e91188c6b
Morten Jensen <morten@citizenme.com>
2015-03-10 21:39:50 +0000

    Fixed cme-oauth-server cookbook, which suddenly broke due to tomcat service restart changes after upgrade

--- cookbooks/cme-oauth-server/metadata.rb
+++ cookbooks/cme-oauth-server/metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'nginx',     '~> 2.4'
-depends 'tomcat',    '~> 0.15'
+depends 'tomcat',    '~> 0.17.3'
