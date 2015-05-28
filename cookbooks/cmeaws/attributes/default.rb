c73f7b60ecfc5fb6c15d293078342ccc33e937a5
Morten Jensen <morten@citizenme.com>
2015-05-28 17:09:15 +0100

    Fixed wrong config for PDX type logging

--- cookbooks/cmeaws/attributes/default.rb
+++ cookbooks/cmeaws/attributes/default.rb
@@ -1,4 +1,4 @@
 default[:cmeaws][:region] = "eu-west-1"
 default[:cmeaws][:log_stream_prefix] = node['hostname']
 default[:cmeaws][:log_group_name] = nil
-default[:cmeaws][:logs] = [ "", "", "", "" ]
+default[:cmeaws][:log_type] = nil # "pdx", "api"
